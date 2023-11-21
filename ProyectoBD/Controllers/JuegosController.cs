42c828628bb3367001ddedecde25cd213dad7033
Obed Toledo de la Rosa <toledo.programmer@hotmail.com>
2023-11-21 07:17:10 -0600

    New controller and model related to games

---
+++ ProyectoBD/Controllers/JuegosController.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using ProyectoBD.Models;
+
+namespace ProyectoBD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("MyCorsImplementationPolicy")]
+
+    public class JuegosController : Controller
+    {
+        MySqlConnection db;
+        public JuegosController(MySqlConnection _db)
+        {
+            db = _db;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> getJuegosDestacados()
+        {
+            
+            IEnumerable<Juegos.Juego> juegosDestacados = await db.QueryAsync<Juegos.Juego>("Call JuegosObtenerJuegosDestacados;");
+
+            return Ok(juegosDestacados);
+        }
+        [HttpGet("[action]")]
+        public async Task<IActionResult> getGeneros()
+        {
+            
+            IEnumerable<Juegos.Genero> generos = await db.QueryAsync<Juegos.Genero>("Call GenerosObtenerGeneros;");
+
+            return Ok(generos);
+        }
+        [HttpPost("[action]")]
+        public async Task<IActionResult> postBuscarJuegos([FromBody] Juegos.postBuscarJuegos model)
+        {
+            
+            IEnumerable<Juegos.Juego> generos = await db.QueryAsync<Juegos.Juego>("Call JuegosBuscarJuegos(@slug);", model);
+
+            return Ok(generos);
+        }
+    }
+}
---
+++ ProyectoBD/Models/Juegos.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace ProyectoBD.Models
+{
+    public class Juegos
+    {
+        public class Juego
+        {
+            public int id { get; set; }
+            public string nombre { get; set; }
+            public string ? descripcion { get; set; }
+            public float? precio { get; set; }
+            public float? descuento { get; set; }
+            public string esbr { get; set; }
+            public float calificacion { get; set; }
+            public string? imagen_portada { get; set; }
+            public string imagen_fondo { get; set; }
+            public string fecha_lanzamiento { get; set; }
+        }
+        public class Genero
+        {
+            public int id { get; set; }
+            public string nombre { get; set; }
+            public string imagen { get; set; }
+        }
+
+        public class postBuscarJuegos
+        {
+            public string slug { get; set; }
+        }
+    }
+}
