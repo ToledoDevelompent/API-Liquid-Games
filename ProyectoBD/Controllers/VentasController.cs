f7bd61b2ee0bdf7ba12937a74d6ad531147d9ddb
Obed Toledo de la Rosa <toledo.programmer@hotmail.com>
2023-11-27 08:55:50 -0600

    Primer End point realizar venta por 1 juego

---
+++ ProyectoBD/Controllers/VentasController.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using ProyectoBD.Models;
+
+namespace ProyectoBD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("MyCorsImplementationPolicy")]
+    public class VentasController : Controller
+    {
+
+        MySqlConnection db;
+        public VentasController(MySqlConnection _db)
+        {
+            db = _db;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> postVentaJuego([FromBody] Ventas.postVentaJuego model)
+        {
+
+            IEnumerable<Juegos.Juego> juego = await db.QueryAsync<Juegos.Juego>("Call BibliotecaVerificarJuegoEspecifico(@idUsuario, @idJuego);", model);
+
+            if (juego == null)
+            {
+                return Ok(false);
+            }
+            else
+            {
+                return Ok(true);
+            }
+        }
+    }
+}
---
+++ ProyectoBD/Models/Ventas.cs
@@ -0,0 +1,16 @@
+﻿namespace ProyectoBD.Models
+{
+    public class Ventas
+    {
+        public class postVentaJuego
+        {
+            public int idUsuario { get; set;}
+            public JuegoForVenta[] juegos { get; set;}
+        }
+
+        public class JuegoForVenta 
+        { 
+            public int IDVenta { get; set;}
+        }
+    }
+}
