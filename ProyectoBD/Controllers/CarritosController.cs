48cd930649571a5b2b79e47777c7f469fd2ef510
Obed Toledo de la Rosa <toledo.programmer@hotmail.com>
2023-12-02 08:53:38 -0600

    Carritos endpoints

---
+++ ProyectoBD/Controllers/CarritosController.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using ProyectoBD.Models;
+
+namespace ProyectoBD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("MyCorsImplementationPolicy")]
+    public class CarritosController : Controller
+    {
+        MySqlConnection db;
+        public CarritosController(MySqlConnection _db)
+        {
+            db = _db;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> postCarritoJuego([FromBody] Carritos.postCarritoJuego model)
+        {
+
+            var juego = await db.QueryFirstAsync("Call CarritosInsertarJuego(@idUsuario, @idJuego);", model);
+            return Ok(true);
+
+            if (juego == null)
+            {
+                return Ok(false);
+            }
+            else
+            {
+                return Ok(true);
+            }
+        }
+    }
+}
---
+++ ProyectoBD/Models/Carritos.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoBD.Models
+{
+    public class Carritos
+    {
+        public class postCarritoJuego
+        {
+            public int idUsuario { get; set; }
+            public int idJuego { get; set; }
+        }
+    }
+}
