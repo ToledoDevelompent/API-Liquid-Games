77a3fc67694b349a106a2701f72073618f6bebf0
Obed Toledo de la Rosa <toledo.programmer@hotmail.com>
2023-12-02 09:31:36 -0600

    Final Endpoints Carrito

--- ProyectoBD/Controllers/CarritosController.cs
+++ ProyectoBD/Controllers/CarritosController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
 using ProyectoBD.Models;
+using System.Collections.Generic;
 
 namespace ProyectoBD.Controllers
 {
@@ -21,17 +22,25 @@ namespace ProyectoBD.Controllers
         public async Task<IActionResult> postCarritoJuego([FromBody] Carritos.postCarritoJuego model)
         {
 
-            var juego = await db.QueryFirstAsync("Call CarritosInsertarJuego(@idUsuario, @idJuego);", model);
+            await db.QueryFirstOrDefaultAsync("Call CarritosInsertarJuego(@idUsuario, @idJuego);", model);
             return Ok(true);
 
-            if (juego == null)
-            {
-                return Ok(false);
-            }
-            else
-            {
-                return Ok(true);
-            }
+        }
+        
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> postVerJuegosEnCarrito([FromBody] Carritos.postCarritoJuego model)
+        {
+
+            IEnumerable < Juegos.Juego > juegos = await db.QueryAsync<Juegos.Juego>("Call CarritosObtenerJuegos(@id);", model);
+            return Ok(juegos);
+        }
+        [HttpPost("[action]")]
+        public async Task<IActionResult> postEliminarCarritoJuego([FromBody] Carritos.postEliminarCarritoJuego model)
+        {
+
+            await db.QueryFirstOrDefaultAsync("Call CarritosEliminarJuego(@idUsuario, @idJuego);", model);
+            return Ok();
         }
     }
 }
--- ProyectoBD/Models/Carritos.cs
+++ ProyectoBD/Models/Carritos.cs
@@ -7,5 +7,14 @@
             public int idUsuario { get; set; }
             public int idJuego { get; set; }
         }
+        public class postVerJuegosEnCarrito
+        {
+            public int id{ get; set; }
+        }
+        public class postEliminarCarritoJuego
+        {
+            public int idUsuario{ get; set; }
+            public int idJuego{ get; set; }
+        }
     }
 }
